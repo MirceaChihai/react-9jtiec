de5ab1b6d44546d29ee975b27f4c636616621571
MirceaChihai <mircea.chihai.99@mail.ru>
2021-11-11 13:12:14 +0200

    1 binaries uploaded

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import './style.css';
 
+
+const arr = [
+  {name = "Carr" ,  imageUrl: "/img/Capture.jpg" }
+]
 export default function App() {
   return (
+    <>
     <div>
   <header>
 <h1> Person SEBN</h1>
-<img src="/Logo/logo.PNG" alt="Logo"/>
+<img src="/img/MAO.PNG" alt="Logo"/>
   </header>
   </div>
+  <div>
+<div>
+  <img src="/img/MAO.jpg" alt="CartPerson"/>
+</div>
+  </div>
+  </>
   )
 }
